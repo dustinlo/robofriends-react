18ef7504ff3cdb732b612299ce886fdc04a1aa64
dustinlo <lo.ch@husky.neu.edu>
2020-06-22 00:02:17 -0700

    Modified js files and added folders

---
+++ src/CardList.js
@@ -0,0 +1,24 @@
+import React from 'react' 
+import Card from './Card'
+
+
+const CardList =({robots})=> {
+    const cardArr = robots.map((user, i)=> {
+        return ( 
+            <Card 
+            key={i} 
+            id={user.id} 
+            name={user.name} 
+            email ={user.email}
+            />
+        )
+    })
+
+    return (
+        <div>
+            {cardArr}
+        </div>
+    ) 
+}
+ 
+export default CardList

f28de9fbf593be1f1686d88a2b496fcad69b5c39
dustinlo <lo.ch@husky.neu.edu>
2020-06-27 01:43:13 -0700

    Change state management to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css'
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [], 
-            searchfield: ''
-        }
+import { setSearchField, requestRobots } from '../actions.js'
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        err: state.requestRobots.err
+        // searchField: state.searchField
     }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+            onSearchChange: (event) => {
+                dispatch(setSearchField(event.target.value))
+            },
+            onRequestRobots: () => dispatch(requestRobots())//requestRobots(dispatch)
+
+    }
+}
+
+class App extends Component {
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users}))
+        this.props.onRequestRobots()
     }
 
-    onSearchChange = (event)=> {
-        this.setState({ searchfield: event.target.value })
-    }
 
     render(){
-        const { robots, searchfield } = this.state
+        const { searchField, onSearchChange, robots, isPending, err } = this.props
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        if (!robots.length){
+        if (isPending){
             return <h1>Loading</h1>
         }else{
         return (
                 <div className = 'tc'>
                     <h1 className ='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots = {filteredRobots}/>
@@ -48,4 +59,4 @@ class App extends Component {
 
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createLogger } from 'redux-logger'
 import App from './containers/App'
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
-//import {robots} from './robots'
+import * as serviceWorker from './serviceWorker'
+import 'tachyons'
+import { searchRobots, requestRobots } from './reducers'
+import thunkMiddleware from 'redux-thunk'
+
+const logger = createLogger()
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-    <App/>, document.getElementById('root'));
+    <Provider store = {store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 ;
 // If you want your app to work offline and load faster, you can change
 // u;nregister() to register() below. Note this comes with some pitfalls.
